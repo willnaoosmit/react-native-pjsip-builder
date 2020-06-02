17b058b63b313b5a934a59b72e95646f26313626
willnaoosmit <55159911+willnaoosmit@users.noreply.github.com>
2020-06-02 16:12:57 -0300

    Update build_pjsip.sh

--- android/build_pjsip.sh
+++ android/build_pjsip.sh
@@ -10,6 +10,7 @@ cat <<EOF > "/tmp/pjsip/pjlib/include/pj/config_site.h"
 #define PJ_CONFIG_ANDROID 1
 #define PJMEDIA_HAS_G729_CODEC 1
 #define PJMEDIA_HAS_G7221_CODEC 1
+#define PJMEDIA_HAS_G711_CODEC 1
 #include <pj/config_site_sample.h>
 #define PJMEDIA_HAS_VIDEO 1
 #define PJMEDIA_AUDIO_DEV_HAS_ANDROID_JNI 0
